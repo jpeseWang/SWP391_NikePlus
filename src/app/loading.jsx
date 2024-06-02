fbff5f1ee1d0fad84633063c6e80c0be6e1a6441
Quang Le Nhat <quang07112004@gmail.com>
2024-06-02 20:52:30 +0700

    debug merge

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,9 @@
 export default function LoadingComponent() {
   return (
-    <div role="status" className="h-screen flex justify-center items-center">
+    <div role="status" className="flex h-screen items-center justify-center">
       <svg
         aria-hidden="true"
-        className="w-20 h-20 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="mr-2 h-20 w-20 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
